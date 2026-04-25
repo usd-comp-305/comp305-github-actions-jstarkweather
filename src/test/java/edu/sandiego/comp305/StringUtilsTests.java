f8b0651e8262758533c069ba47a1dbf8ba6f3dc9
Jet Tang Ro Starkweather <jstarkweather@sandiego.edu>
2026-04-25 14:08:08 -0700

    refractor: changed the name of the test

--- src/test/java/edu/sandiego/comp305/StringUtilsTests.java
+++ src/test/java/edu/sandiego/comp305/StringUtilsTests.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StringUtilsTests {
 
     @Test
-    public void testReverseNormal() {
+    public void testReverseNormalString() {
         assertEquals("cba", StringUtils.reverseString("abc"));
     }
 
